8fc36a89fd3e99b4d9069ad0a072eac9ed4f939f
Celiomar Júnior <celiomarxd@gmail.com>
2024-10-12 11:46:22 -0300

    Adicionar arquivos de projeto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace ConversorApp
+{
+    public partial class MainPage : TabbedPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            var w = selectDistancia.SelectedItem;
+            var x = selectPeso.SelectedItem;
+            var y = selectTemperatura.SelectedItem;
+        }
+         
+   
+    }
+
+}
